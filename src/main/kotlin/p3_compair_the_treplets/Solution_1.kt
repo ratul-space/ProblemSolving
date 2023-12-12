9f7418470aaa48fe8a8e3b88d61d2e0443f0f7f6
ratul-space <to.ratul.hosen@gmail.com>
2023-12-12 22:29:06 +0600

    fix: resolve the logical error

--- src/main/kotlin/p3_compair_the_treplets/Solution_1.kt
+++ src/main/kotlin/p3_compair_the_treplets/Solution_1.kt
@@ -1,21 +1,26 @@
 package p3_compair_the_treplets
 
 fun main() {
-    val a = arrayOf(5, 6, 7)
-    val b = arrayOf(3, 6, 10)
+    val a = arrayOf(1, 2, 3)
+    val b = arrayOf(3, 2, 1)
     val result = compareTriplets(a, b)
+
     println("${result[0]} ${result[1]}")
 }
 
 fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
     var scoreOfAlice = 0
     var scoreOfBob = 0
+
     for (i in 0..2) {
-        if (a[i] > b[i]) scoreOfAlice += 1
-        if (a[i] > b[i]) scoreOfBob += 1
+        if (a[i] > b[i]) {
+            scoreOfAlice += 1
+        }
+
+        if (a[i] < b[i]){
+            scoreOfBob += 1
+        }
     }
-
-
 
     return arrayOf(scoreOfAlice, scoreOfBob)
 }
