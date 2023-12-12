25744265f072cc58e9d2dbbec49f575df46659f1
ratul-space <to.ratul.hosen@gmail.com>
2023-12-12 14:57:51 +0600

    feat: sum of diffrent eliments

---
+++ src/main/kotlin/hackerrank/p2_simpleArraySum/Solution1.kt
@@ -0,0 +1,14 @@
+package hackerrank.p2_simpleArraySum
+
+fun main() {
+    val sum = simpleArraySum(arrayOf(2, 4, 6, 10, 6))
+    println(sum)
+}
+
+fun simpleArraySum(ar: Array<Int>): Int {
+    var sum = 0
+    for (eliment in ar) {
+        sum += eliment
+    }
+    return sum
+}
