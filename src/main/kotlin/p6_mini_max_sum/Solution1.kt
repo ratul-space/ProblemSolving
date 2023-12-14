ee8d254df076a51ca187463b6dd29d9b629a0c9d
ratul-space <to.ratul.hosen@gmail.com>
2023-12-14 12:37:23 +0600

    feat: problem solving

---
+++ src/main/kotlin/p6_mini_max_sum/Solution1.kt
@@ -0,0 +1,18 @@
+package p6_mini_max_sum
+
+fun main() {
+
+}
+
+fun miniMaxSum(arr: Array<Int>) {
+    var min = Int.MAX_VALUE
+    var max = 0
+    var sum = 0L
+
+    for (number in arr) {
+        sum += number
+        if (number < min) min = number
+        if (number > max) max = number
+    }
+    println("${sum - max} ${sum - min}")
+}
