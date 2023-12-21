8e6cdbc3669a1ed279410f4af4ec48b3092b8d83
ratul-space <to.ratul.hosen@gmail.com>
2023-12-21 22:35:45 +0600

    feat: solved countApplesAndOranges problem

---
+++ src/main/kotlin/p10_apple_and_orange/Solution1.kt
@@ -0,0 +1,34 @@
+package p10_apple_and_orange
+
+fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: Array<Int>, oranges: Array<Int>): Unit {
+    // Write your code here
+    println(apples.count { it + a in s..t })
+    println(oranges.count { it + b in s..t })
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val s = first_multiple_input[0].toInt()
+
+    val t = first_multiple_input[1].toInt()
+
+    val second_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val a = second_multiple_input[0].toInt()
+
+    val b = second_multiple_input[1].toInt()
+
+    val third_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val m = third_multiple_input[0].toInt()
+
+    val n = third_multiple_input[1].toInt()
+
+    val apples = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val oranges = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+}
+
